6fffb2f44340b5fb13dfae52076aebcb77309d8b
Joe Ortega <joeortega79@gmail.com>
2018-05-03 08:19:15 +0930

    0.0.001
    Board Class and Tile class added.  A tile board is generated on pressing play button in editor.

---
+++ UrsoTrio/Assets/Scripts/Board.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	public int width;
+	public int height;
+
+	public GameObject tilePrefab;
+
+	Tile[,] m_allTiles;
+
+	// Use this for initialization
+	void Start () {
+		m_allTiles = new Tile[width, height];
+		SetupTiles ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SetupTiles()
+	{
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				GameObject tile = Instantiate (tilePrefab, new Vector3 (i, j, 0), Quaternion.identity) as GameObject;
+				tile.name = "Title (" + i + "," + i + ")";
+
+				m_allTiles [i, j] = tile.GetComponent<Tile> ();
+				tile.transform.parent = transform;
+			}
+		}
+	}
+}
