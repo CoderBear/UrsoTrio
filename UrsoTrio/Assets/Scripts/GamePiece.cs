89c3b7c2cc11326a6eedc86b5cedd281db2225db
Joe Ortega <joeortega79@gmail.com>
2018-05-03 10:26:26 +0930

    0.0.005
    Tiles can now move, at least as a group but function is there to extended individually.

--- UrsoTrio/Assets/Scripts/GamePiece.cs
+++ UrsoTrio/Assets/Scripts/GamePiece.cs
@@ -7,6 +7,8 @@ public class GamePiece : MonoBehaviour {
 	public int xIndex;
 	public int yIndex;
 
+	bool m_isMoving = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,11 +16,54 @@ public class GamePiece : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Move ((int)transform.position.x + 1, (int)transform.position.y, 0.5f);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Move ((int)transform.position.x - 1, (int)transform.position.y, 0.5f);
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Move ((int)transform.position.x, (int)transform.position.y + 1, 0.5f);
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Move ((int)transform.position.x, (int)transform.position.y - 1, 0.5f);
+		}
 	}
 
 	public void SetCoord(int x, int y) {
 		xIndex = x;
 		yIndex = y;
+	}
+
+	public void Move(int destX, int destY, float timeToMove) {
+		if (!m_isMoving) {
+			StartCoroutine (MoveRoutine (new Vector3 (destX, destY, 0), timeToMove));
+		}
+	}
+
+	IEnumerator MoveRoutine(Vector3 destination, float timeToMove) {
+		Vector3 startPosition = transform.position;
+		bool reachDestination = false;
+		float elapsedTime = 0f;
+		m_isMoving = true;
+
+		while (!reachDestination) {
+			if (Vector3.Distance (transform.position, destination) < 0.01f) {
+				reachDestination = true;
+				transform.position = destination;
+				SetCoord ((int)destination.x, (int)destination.y);
+				break;
+			}
+
+			elapsedTime += Time.deltaTime;
+
+			float t = elapsedTime / timeToMove;
+
+			transform.position = Vector3.Lerp (startPosition, destination, t);
+
+			yield return null;
+		}
+
+		m_isMoving = false;
 	}
 }
