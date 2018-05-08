f9985719a6b72871570587c2e2fb285846735af0
Joe Ortega <joeortega79@gmail.com>
2018-05-09 07:38:08 +0930

    0.001.006
    Put in place code for Row, Columbs and Adajcent Bombs.
    
    Created the Ring glow particle Effect.
    
    Create a RowBomb prefab

---
+++ UrsoTrio/Assets/Scripts/Bomb.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BombType
+{
+	Non,
+	Column,
+	Row,
+	Adajcent,
+	Color
+}
+
+public class Bomb : GamePiece 
+{
+	public BombType bombType;
+}
