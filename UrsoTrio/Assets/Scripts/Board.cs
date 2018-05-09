0bf54c71e3e1438937cf7b79c27e99652b9773e8
Joe Ortega <joeortega79@gmail.com>
2018-05-09 09:39:12 +0930

    0.001.007
    All bomb prefabs are implemeted.
    
    The code niow clears when the bomb is used.

--- UrsoTrio/Assets/Scripts/Board.cs
+++ UrsoTrio/Assets/Scripts/Board.cs
@@ -690,6 +690,10 @@ public class Board : MonoBehaviour {
 		bool isFinished = false;
 
 		while (!isFinished) {
+
+			List<GamePiece> bombedPieces = GetBombedPieces (gamePieces);
+			gamePieces = gamePieces.Union (bombedPieces).ToList ();
+
 			ClearPieceAt (gamePieces);
 			BreakTileAt (gamePieces); 
 
@@ -780,6 +784,41 @@ public class Board : MonoBehaviour {
 			}
 		}
 		return gamePieces;
+	}
+
+	List<GamePiece> GetBombedPieces(List<GamePiece> gamePieces)
+	{
+		List<GamePiece> allPiecesToClear = new List<GamePiece> ();
+		foreach(GamePiece piece in gamePieces)
+		{
+			if(piece != null)
+			{
+				List<GamePiece> piecesToClear = new List<GamePiece> ();
+
+				Bomb bomb = piece.GetComponent<Bomb> ();
+
+				if(bomb != null)
+				{
+					switch (bomb.bombType) 
+					{
+					case BombType.Column:
+						piecesToClear = GetColumnPieces (bomb.xIndex);
+						break;
+					case BombType.Row:
+						piecesToClear = GetRowPieces (bomb.yIndex);
+						break;
+					case BombType.Adajcent:
+						piecesToClear = GetAdjacentPieces (bomb.xIndex, bomb.yIndex, 1);
+						break;
+					case BombType.Color:
+						break;
+					}
+
+					allPiecesToClear = allPiecesToClear.Union (piecesToClear).ToList ();
+				}
+			}
+		}
+		return allPiecesToClear;
 	}
 	#endregion
 
